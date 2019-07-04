e83d6c2b07893b65ea0152dce12ff497d5808c3f
candra saputra <candrasaputra@live.com>
2019-07-04 15:33:06 +0700

    fix output peran

--- Selasa/exercises-3.js
+++ Selasa/exercises-3.js
@@ -1,6 +1,6 @@
 let nama = 'candra';
 // Ksatria, Tabib, dan Penyihir
-let peran = '';
+let peran = 'Ksatriax';
 
 if(nama === ''){
     console.log('Nama harus diisi!');
@@ -8,5 +8,14 @@ if(nama === ''){
     console.log('Halo '+nama+', Pilih Peranmu untuk memulai game!');
 } else {
     console.log('Selamat datang di Dunia Proxytia, '+nama);
-    console.log('Halo '+peran+' ,'+nama+' kamu dapat menyerang dengan senjatamu!');
+    
+    if (peran === 'Ksatria') {
+        console.log('Halo '+peran+' ,'+nama+' kamu dapat menyerang dengan senjatamu!');
+    } else if (peran === 'Tabib') {
+        console.log('Halo '+peran+' ,'+nama+' kamu akan membantu temanmu yang terluka.');
+    } else if (peran === 'Penyihir') {
+        console.log('Halo '+peran+' ,'+nama+' ciptakan keajaiban yang membantu kemenanganmu!');
+    } else {
+        console.log('Maaf '+nama+' peran '+peran+' tidak tersedia silahkan pilih peran Ksatria, Tabib, atau Penyihir!');
+    }
 }

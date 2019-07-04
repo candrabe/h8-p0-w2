c8cf955a62b6f2986138134b273bfcaa150b99f6
candra saputra <candrasaputra@live.com>
2019-07-04 17:02:37 +0700

    fix rabu

--- Rabu/exercises-6.js
+++ Rabu/exercises-6.js
@@ -24,6 +24,8 @@ while(j > 0){
 * Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for.
 * Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan menghitung mundur.
 * Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+* Hints:
+* Operator ++ disebut dengan operator Increment Operator -- disebut dengan operator Decrement.
 */
 
 console.log('LOOPING PERTAMA');

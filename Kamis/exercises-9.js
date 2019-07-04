462502cfd9e199116f43769d17d073f1b32d132b
candra saputra <candrasaputra@live.com>
2019-07-04 17:19:06 +0700

    fix kamis

--- Kamis/exercises-9.js
+++ Kamis/exercises-9.js
@@ -7,7 +7,7 @@ function shoutOut(){
     return "Halo Function!";
 }
 
-console.log(shoutOut()) // Menampilkan "Halo Function!" di console
+console.log(shoutOut()); // Menampilkan "Halo Function!" di console
 
 /*
 * Tugas 2
@@ -19,7 +19,7 @@ function calculateMultiply(num1,num2){
 }
 
 var num1 = 5;
-var num2 = 6;
+var num2 = '6';
 
 var hasilPerkalian = calculateMultiply(num1,num2);
 console.log(hasilPerkalian); // Menampilkan angka 30
